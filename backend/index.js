594f794e974a3ffc3fe01e0594a9faec885cdbfb
Mateusz Stapaj <mtxstapaj@gmail.com>
2022-10-22 18:47:07 +0200

    fix dev docker compose

--- backend/index.js
+++ backend/index.js
@@ -1,36 +1,36 @@
-const express = require("express");
-const cors = require("cors");
-const mongoose = require("mongoose");
-const http = require("http");
-const users = require("./routes/users");
-const exercises = require("./routes/exercises");
-const comments = require("./routes/comments");
-const swaggerUi = require("swagger-ui-express");
-const swaggerDocument = require("./swagger.json");
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const http = require('http');
+const users = require('./routes/users');
+const exercises = require('./routes/exercises');
+const comments = require('./routes/comments');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
 
 const app = express();
 
 app.use(express.json());
 app.use(
     cors({
-        origin: "*",
-        methods: ["GET", "POST", "DELETE", "UPDATE", "PUT"],
+        origin: '*',
+        methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT'],
     })
 );
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-app.use("/users", users);
-app.use("/exercises", exercises);
-app.use("/comments", comments);
+app.use('/users', users);
+app.use('/exercises', exercises);
+app.use('/comments', comments);
 
-require("dotenv").config();
+require('dotenv').config();
 
 const dbConnData = {
-    host: process.env.MONGO_HOST || "127.0.0.1",
+    host: process.env.MONGO_HOST || '127.0.0.1',
     port: process.env.MONGO_PORT || 27017,
-    database: process.env.MONGO_DATABASE || "local",
-    user: process.env.MONGO_USERNAME || "user",
-    password: process.env.MONGO_PASSWORD || "secr3t",
+    database: process.env.MONGO_DATABASE || 'local',
+    user: process.env.MONGO_USERNAME || 'user',
+    password: process.env.MONGO_PASSWORD || 'secr3t',
 };
 
 mongoose
