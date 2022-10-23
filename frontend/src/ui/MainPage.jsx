5d14ff4bfb0833f70302367c00200a85880d4ec6
Alancio <alannadolnyy@gmail.com>
2022-10-23 19:18:40 +0200

    add main page

--- frontend/src/ui/MainPage.jsx
+++ frontend/src/ui/MainPage.jsx
@@ -1,5 +1,31 @@
 import React from 'react';
 
-const MainPage = () => <div>Here should be the main page</div>;
+import codeGif from '../styles/6iE3BC564u.gif';
+
+const MainPage = () => (
+  <div className="main-page-container">
+    <nav className="main-page">
+      <ul>
+        <li>
+          <a>Code</a>
+          <div className="border"></div>
+        </li>
+        <li>
+          <a>Learn</a>
+          <div className="border"></div>
+        </li>
+        <li>
+          <a>Train</a>
+          <div className="border"></div>
+        </li>
+        <li>
+          <a>Compete</a>
+          <div className="border"></div>
+        </li>
+      </ul>
+    </nav>
+    <img src={codeGif} alt="gif with code" />
+  </div>
+);
 
 export default MainPage;
