30c5ff2af3740f306c5e3e7c2135a9676de26fa1
Mateusz Stapaj <mtxstapaj@gmail.com>
2022-11-02 21:08:38 +0100

    edit java execute

--- backend_containers/routes/containers.js
+++ backend_containers/routes/containers.js
@@ -163,4 +163,35 @@ router.post('/r', async (req, res) => {
     }
 });
 
+router.post('/java', async (req, res) => {
+    try {
+        let data = req.body;
+        if (data.args) {
+            const splitedData = data.toExecute.split(
+                'public static void main(String[] args) {'
+            );
+            data.toExecute =
+                splitedData[0] +
+                'public static void main(String[] args) {' +
+                '\n System.out.println(' +
+                data.func +
+                '(' +
+                data.args.join(', ') +
+                '));' +
+                splitedData[1];
+        }
+        const response = await axios.post('http://java:6004', {
+            toExecute: data.toExecute,
+        });
+        return res.status(200).send(response.data);
+    } catch (error) {
+        if (error.response && error.response.data.stderr) {
+            return res.status(202).send({ output: error.response.data.stderr });
+        } else {
+            console.log(error);
+            res.status(500).send(error);
+        }
+    }
+});
+
 module.exports = router;
