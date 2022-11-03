372a9b9bd3d72be1120d245751b78199223b418c
Mateusz Stapaj <mtxstapaj@gmail.com>
2022-11-03 12:40:51 +0100

    edit cpp, c execute

--- backend_containers/routes/containers.js
+++ backend_containers/routes/containers.js
@@ -2,76 +2,6 @@ const axios = require('axios');
 const express = require('express');
 const router = express.Router();
 
-const programmingLanguagesNormal = ['javascript', 'python', 'bash', 'r'];
-const programmingLanguagesAnother = [
-    { lang: 'java', spliter: 'public static void main(String[] args){' },
-    { lang: 'cpp', spliter: 'int main() {' },
-    { lang: 'c', spliter: 'int main() {' },
-    ,
-];
-
-// programmingLanguagesNormal.map((n) =>
-//     router.post('/' + n, async (req, res) => {
-//         try {
-//             let data = req.body;
-//             if (data.args) {
-//                 data.toExecute = data.args + '\n' + data.toExecute;
-//             }
-//             const response = await axios.post(
-//                 'http://' + n + ':600' + programmingLanguagesNormal.indexOf(n),
-//                 { toExecute: data.toExecute }
-//             );
-//             return res.status(200).send(response.data);
-//         } catch (error) {
-//             if (error.response && error.response.data.stderr) {
-//                 return res
-//                     .status(202)
-//                     .send({ output: error.response.data.stderr });
-//             } else {
-//                 console.log(error);
-//                 res.status(500).send(error);
-//             }
-//         }
-//     })
-// );
-
-// programmingLanguagesAnother.map((n) => {
-//     router.post('/' + n.lang, async (req, res) => {
-//         try {
-//             let data = req.body;
-//             if (data.args) {
-//                 const splitedData = data.toExecute.split(n.spliter);
-//                 data.toExecute =
-//                     splitedData[0] +
-//                     n.spliter +
-//                     data.args +
-//                     '\n' +
-//                     splitedData[1];
-//             }
-//             const response = await axios.post(
-//                 'http://' +
-//                     n.lang +
-//                     ':600' +
-//                     (parseInt(programmingLanguagesNormal.length) +
-//                         parseInt(programmingLanguagesAnother.indexOf(n))),
-//                 {
-//                     toExecute: data.toExecute,
-//                 }
-//             );
-//             return res.status(200).send(response.data);
-//         } catch (error) {
-//             if (error.response && error.response.data.stderr) {
-//                 return res
-//                     .status(202)
-//                     .send({ output: error.response.data.stderr });
-//             } else {
-//                 console.log(error);
-//                 res.status(500).send(error);
-//             }
-//         }
-//     });
-// });
-
 router.post('/javascript', async (req, res) => {
     try {
         let data = req.body;
@@ -181,6 +111,66 @@ router.post('/java', async (req, res) => {
                 splitedData[1];
         }
         const response = await axios.post('http://java:6004', {
+            toExecute: data.toExecute,
+        });
+        return res.status(200).send(response.data);
+    } catch (error) {
+        if (error.response && error.response.data.stderr) {
+            return res.status(202).send({ output: error.response.data.stderr });
+        } else {
+            console.log(error);
+            res.status(500).send(error);
+        }
+    }
+});
+
+router.post('/cpp', async (req, res) => {
+    try {
+        let data = req.body;
+        if (data.args) {
+            const splitedData = data.toExecute.split('return 0;');
+            const lastElem = splitedData.pop();
+            data.toExecute =
+                splitedData.join(' \n') +
+                ' cout << ' +
+                data.func +
+                '(' +
+                data.args.join(', ') +
+                ');\n' +
+                'return 0;' +
+                lastElem;
+        }
+        const response = await axios.post('http://cpp:6005', {
+            toExecute: data.toExecute,
+        });
+        return res.status(200).send(response.data);
+    } catch (error) {
+        if (error.response && error.response.data.stderr) {
+            return res.status(202).send({ output: error.response.data.stderr });
+        } else {
+            console.log(error);
+            res.status(500).send(error);
+        }
+    }
+});
+
+router.post('/c', async (req, res) => {
+    try {
+        let data = req.body;
+        if (data.args) {
+            const splitedData = data.toExecute.split('return 0;');
+            const lastElem = splitedData.pop();
+            data.toExecute =
+                splitedData.join(' \n') +
+                ' printf("%d", ' +
+                data.func +
+                '(' +
+                data.args.join(', ') +
+                '));\n' +
+                'return 0;' +
+                lastElem;
+        }
+        const response = await axios.post('http://c:6006', {
             toExecute: data.toExecute,
         });
         return res.status(200).send(response.data);
