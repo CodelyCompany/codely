e6e402a80ef89bc018765a8a709471b7172f0f5b
Mateusz Stapaj <mtxstapaj@gmail.com>
2022-11-05 17:04:50 +0100

    add files for containers

---
+++ kubernetes/bin/build-and-push.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+containers=(bash c cpp java javascript python r)
+
+cd ../../
+
+for item in ${containers[*]}
+do
+    docker build --tag mtx22/codely-$item containers/$item/
+    docker push mtx22/codely-$item
+done
+
+
+docker build --tag mtx22/codely-backend backend/
+docker push mtx22/codely-backend
+
+docker build --tag mtx22/codely-frontend frontend/
+docker push mtx22/codely-frontend
+
+docker build --tag mtx22/codely-backend-containers backend_containers/
+docker push mtx22/codely-backend-containers
