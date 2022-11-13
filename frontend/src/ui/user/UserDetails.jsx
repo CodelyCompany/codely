df4822b2e79ed44eb48616baf9709138faba7959
dpielecki <dpielecki19@gmail.com>
2022-11-13 19:20:36 +0100

    comments -> reviews part 2

--- frontend/src/ui/user/UserDetails.jsx
+++ frontend/src/ui/user/UserDetails.jsx
@@ -12,7 +12,7 @@ import { getUserByUsername } from '../../ducks/user/selectors';
 
 import SectionWrapper from './SectionWrapper';
 import UserExercisesList from './UserExercisesList';
-import WrittenComments from './WrittenComments';
+import WrittenReviews from './WrittenReviews';
 
 const UserDetails = ({ GetUsers }) => {
     const { user, getAccessTokenSilently } = useAuth0();
@@ -78,10 +78,10 @@ const UserDetails = ({ GetUsers }) => {
                         />
                     </SectionWrapper>
                     <SectionWrapper
-                        mode="comments"
-                        condition={!_.isEmpty(foundUser.writtenComments)}
+                        mode="reviews"
+                        condition={!_.isEmpty(foundUser.writtenReviews)}
                     >
-                        <WrittenComments comments={foundUser.writtenComments} />
+                        <WrittenReviews reviews={foundUser.writtenReviews} />
                     </SectionWrapper>
                 </Box>
             )}
