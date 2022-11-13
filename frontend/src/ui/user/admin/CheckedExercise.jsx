ce02322c18ee830495b61eb30bac91f1087da558
Alancio <alannadolnyy@gmail.com>
2022-11-13 21:12:58 +0100

    add checked exercises to admin panel

--- frontend/src/ui/user/admin/CheckedExercise.jsx
+++ frontend/src/ui/user/admin/CheckedExercise.jsx
@@ -1,9 +1,82 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-import { Box } from '@mui/material';
+import { useAuth0 } from '@auth0/auth0-react';
+import { Card, Typography } from '@mui/material';
+import { DataGrid } from '@mui/x-data-grid';
+import * as _ from 'lodash';
+import { PropTypes } from 'prop-types';
+import { connect } from 'react-redux';
 
-function CheckedExercise() {
-  return <Box sx={{ width: '50%' }}>CheckedExercise</Box>;
+import { GetExercises } from '../../../ducks/exercises/operations';
+import { getExercisesFromState } from '../../../ducks/exercises/selectors';
+
+import ExerciseDialog from './ExerciseDialog';
+
+const columns = [
+  {
+    field: 'title',
+    headerName: 'Title',
+    flex: 1,
+  },
+  {
+    field: 'author',
+    headerName: 'Author',
+    flex: 1,
+  },
+];
+
+function CheckedExercise({ checkedExercises, GetExercises }) {
+  const { getAccessTokenSilently } = useAuth0();
+  const rows = checkedExercises ? [...checkedExercises] : [];
+
+  useEffect(() => {
+    if (_.isEmpty(checkedExercises))
+      (async () => {
+        const token = await getAccessTokenSilently({
+          audience: `${
+            process.env.REACT_APP_BACKEND || 'http://localhost:5000'
+          }`,
+        });
+        await GetExercises(token);
+      })();
+  }, []);
+
+  return (
+    <Card
+      sx={{ height: '500px', width: '50%', margin: '10px', padding: '10px' }}
+    >
+      <Typography
+        color='primary'
+        variant='h6'
+        sx={{ fontWeight: 'bolder', textAlign: 'center' }}
+      >
+        Checked exercises
+      </Typography>
+      <DataGrid
+        sx={{ width: 'calc(100% - 20px)', height: '400px', margin: '10px' }}
+        getRowId={(row) => row._id}
+        rows={rows.map((row) => ({ ...row, author: row.author.username }))}
+        columns={columns}
+        pageSize={5}
+        rowsPerPageOptions={[5]}
+        disableSelectionOnClick
+        experimentalFeatures={{ newEditingApi: true }}
+      />
+    </Card>
+  );
 }
 
-export default CheckedExercise;
+const mapStateToProps = (state) => ({
+  checkedExercises: getExercisesFromState(state),
+});
+
+const mapDispatchToProps = {
+  GetExercises,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(CheckedExercise);
+
+CheckedExercise.propTypes = {
+  checkedExercises: PropTypes.array,
+  GetExercises: PropTypes.func,
+};
