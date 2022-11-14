437315167f5f5d2f9f254e098ca9f2dd8157bf60
Mateusz Stapaj <mtxstapaj@gmail.com>
2022-11-14 23:37:23 +0100

    mongo localpath fix

--- bin/start-development.sh
+++ bin/start-development.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if  [ ! -d "/tmp/mongodb/database" ]; then 
-    mkdir -p /tmp/mongodb/database
-    echo Created required /tmp/mongodb/database folder
+if  [ ! -d "/home/${USER}/mongodb/database" ]; then 
+    mkdir -p /home/${USER}/mongodb/database
+    echo Created required /home/${USER}/mongodb/database folder
 fi
 
 docker compose --file ../docker/dev/docker-compose.yml up --build 
