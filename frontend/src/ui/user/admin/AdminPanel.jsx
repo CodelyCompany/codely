f4f3147d041b18e86c8fb1803b1fa470aeeef155
Alancio <alannadolnyy@gmail.com>
2022-11-14 23:11:26 +0100

    add forbidden for admin panel

--- frontend/src/ui/user/admin/AdminPanel.jsx
+++ frontend/src/ui/user/admin/AdminPanel.jsx
@@ -2,18 +2,21 @@ import React from 'react';
 
 import { Box } from '@mui/material';
 
+import AdminPanelWrapper from './AdminPanelWrapper';
 import AllUsers from './AllUsers';
 import CheckedExercise from './CheckedExercise';
 import ExerciseToCheck from './ExerciseToCheck';
 
 const AdminPanel = () => (
-  <Box sx={{ width: '100%', height: '100%' }}>
-    <Box sx={{ wdith: '100%', display: 'flex' }}>
-      <ExerciseToCheck />
-      <CheckedExercise />
+  <AdminPanelWrapper>
+    <Box sx={{ width: '100%', height: '100%' }}>
+      <Box sx={{ wdith: '100%', display: 'flex' }}>
+        <ExerciseToCheck />
+        <CheckedExercise />
+      </Box>
+      <AllUsers />
     </Box>
-    <AllUsers />
-  </Box>
+  </AdminPanelWrapper>
 );
 
 export default AdminPanel;
