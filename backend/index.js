5fc2c3dd4982abe30aa544552107b0307955caeb
Mateusz Stapaj <mtxstapaj@gmail.com>
2022-11-15 21:50:59 +0100

    test single quote

--- backend/index.js
+++ backend/index.js
@@ -1,38 +1,38 @@
-const express = require("express");
-const cors = require("cors");
-const mongoose = require("mongoose");
-const http = require("http");
-const users = require("./routes/users");
-const exercises = require("./routes/exercises");
-const reviews = require("./routes/reviews");
-const swaggerUi = require("swagger-ui-express");
-const swaggerDocument = require("./swagger.json");
-const jwtCheck = require("./auth");
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+const http = require('http');
+const users = require('./routes/users');
+const exercises = require('./routes/exercises');
+const reviews = require('./routes/reviews');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+const jwtCheck = require('./auth');
 
 const app = express();
 
 app.use(express.json());
 app.use(
   cors({
-    origin: "*",
-    methods: ["GET", "POST", "DELETE", "UPDATE", "PUT"],
+    origin: '*',
+    methods: ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT'],
   })
 );
 // app.use(jwtCheck);
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
-app.use("/users", users);
-app.use("/exercises", exercises);
-app.use("/reviews", reviews);
+app.use('/users', users);
+app.use('/exercises', exercises);
+app.use('/reviews', reviews);
 
-require("dotenv").config();
+require('dotenv').config();
 
 const dbConnData = {
-  host: process.env.MONGO_HOST || "127.0.0.1",
+  host: process.env.MONGO_HOST || '127.0.0.1',
   port: process.env.MONGO_PORT || 27017,
-  database: process.env.MONGO_DATABASE || "local",
-  user: process.env.MONGO_USERNAME || "user",
-  password: process.env.MONGO_PASSWORD || "secr3t",
+  database: process.env.MONGO_DATABASE || 'local',
+  user: process.env.MONGO_USERNAME || 'user',
+  password: process.env.MONGO_PASSWORD || 'secr3t',
 };
 
 mongoose
