1a0ea64e4655a219d6520a5052e17a6a38aa3590
Alancio <alannadolnyy@gmail.com>
2022-11-25 17:54:43 +0100

    moving two players to game room

--- backend/peerServer.js
+++ backend/peerServer.js
@@ -21,7 +21,30 @@ const io = socket(server, {
   },
 });
 
+// const joinGame = () => {
+//   const rooms = io.of('/').adapter.rooms;
+//   const id = Array.from(rooms.get('/waiting'))[0];
+//   console.log(io.sockets.adapter.nsp.sockets.get(id));
+//   io.sockets.adapter.nsp.sockets.get(id).join('/game');
+//   console.log(rooms.get('/game'));
+// }
+
+const joinGame = () => {
+  const rooms = io.of('/').adapter.rooms;
+  const ids = Array.from(rooms.get('/waiting'));
+  if (ids.length >= 2) {
+    const users = io.sockets.adapter.nsp.sockets;
+    users.get(ids[0]).leave('/waiting');
+    users.get(ids[1]).leave('/waiting');
+    users.get(ids[0]).join('/game');
+    users.get(ids[1]).join('/game');
+  }
+  console.log(rooms.get('/game'));
+  console.log(rooms.get('/waiting'));
+};
+
 io.on('connection', (socket) => {
+  socket.join('/waiting');
   socket.emit('players', Array.from(io.sockets.sockets.keys()).length);
   socket.broadcast.emit(
     'players',
@@ -34,4 +57,5 @@ io.on('connection', (socket) => {
       Array.from(io.sockets.sockets.keys()).length
     );
   });
+  joinGame();
 });
