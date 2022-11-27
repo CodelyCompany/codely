dfb32f4d60502acd61c4c5844a23dc6a0e43795c
Alancio <alannadolnyy@gmail.com>
2022-11-27 16:58:09 +0100

    add form and validation

---
+++ frontend/src/ui/versus/ChooseExerciseLang.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+
+import { Checkbox, FormControlLabel, Paper, Typography } from '@mui/material';
+import { PropTypes } from 'prop-types';
+
+const ChooseExerciseLang = ({ formik }) => {
+  const languages = ['JavaScript', 'Bash', 'C', 'C++', 'Java', 'Python', 'R'];
+
+  return (
+    <Paper elevation={3} sx={{ padding: '10px', margin: '10px 0' }}>
+      <Typography variant="h6" color="primary" fontWeight={'bolder'}>
+        Pick languages your languages
+      </Typography>
+      <form>
+        <div role="group" aria-labelledby="checkbox-group">
+          {languages.map((el) => (
+            <FormControlLabel
+              key={el}
+              control={
+                <Checkbox
+                  id={el}
+                  name="checked"
+                  value={el === 'C++' ? 'cpp' : el.toLowerCase()}
+                  onChange={formik.handleChange}
+                />
+              }
+              label={
+                <span style={{ position: 'relative', top: '3px' }}>{el}</span>
+              }
+            />
+          ))}
+        </div>
+        <Typography fontWeight={'bolder'} color="error">
+          {formik.errors && formik.errors.checked}
+        </Typography>
+      </form>
+    </Paper>
+  );
+};
+
+export default ChooseExerciseLang;
+
+ChooseExerciseLang.propTypes = {
+  formik: PropTypes.object.isRequired,
+};
