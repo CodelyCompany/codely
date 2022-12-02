8275d8b71a1fa52e70ed9079e30a78348f1b5c0a
Alancio <alannadolnyy@gmail.com>
2022-12-03 00:02:30 +0100

    finish dark theme

--- frontend/src/ui/MainPage.jsx
+++ frontend/src/ui/MainPage.jsx
@@ -1,31 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import codeGif from '../styles/6iE3BC564u.gif';
+import codeGifDarkTheme from '../styles/0082BUw8UC.gif';
 
-const MainPage = () => (
-  <div className='main-page-container'>
-    <nav className='main-page'>
-      <ul>
-        <li>
-          <a>Code</a>
-          <div className='border'></div>
-        </li>
-        <li>
-          <a>Learn</a>
-          <div className='border'></div>
-        </li>
-        <li>
-          <a>Train</a>
-          <div className='border'></div>
-        </li>
-        <li>
-          <a>Compete</a>
-          <div className='border'></div>
-        </li>
-      </ul>
-    </nav>
-    <img src={codeGif} alt='gif with code' />
-  </div>
-);
+const MainPage = () => {
+  const [theme, setTheme] = useState(0);
 
+  useEffect(() => {
+    setTheme(parseInt(localStorage.getItem('theme')) ?? 0);
+  }, []);
+
+  return (
+    <div className={`main-page-container main-theme-${theme}`}>
+      <nav className='main-page'>
+        <ul>
+          <li>
+            <a>Code</a>
+            <div className='border'></div>
+          </li>
+          <li>
+            <a>Learn</a>
+            <div className='border'></div>
+          </li>
+          <li>
+            <a>Train</a>
+            <div className='border'></div>
+          </li>
+          <li>
+            <a>Compete</a>
+            <div className='border'></div>
+          </li>
+        </ul>
+      </nav>
+      <img src={theme ? codeGifDarkTheme : codeGif} alt='gif with code' />
+    </div>
+  );
+};
 export default MainPage;
