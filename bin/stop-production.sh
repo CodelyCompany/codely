dc00d1df6935afb3d6ad7e0c7c776ac0983747a8
Mateusz Stapaj <mtxstapaj@gmail.com>
2022-12-03 21:48:56 +0100

    add kubernetes dashboard

--- bin/stop-production.sh
+++ bin/stop-production.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+kubectl delete -f https://raw.githubusercontent.com/kubernetes/dashboard/v2.6.1/aio/deploy/recommended.yaml
 kubectl delete -f ../kubernetes/production --recursive
---
+++ kubernetes/production/README.md
@@ -0,0 +1,7 @@
+## Uruchomienie dashboardu
+
+`kubectl proxy`
+
+## Wygenerowanie tokena dla admina do dashboardu
+
+`kubectl -n production create token admin-user`
