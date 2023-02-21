94b1fc9db96f2898d866a0019259d25d006ef54e
Alan Nadolny <alannadolnyy@gmail.com>
2023-02-22 00:35:13 +0100

    Add jsconfig for absolute paths and install vite

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 
 import { Auth0Provider } from '@auth0/auth0-react';
 import ReactDOM from 'react-dom/client';
-import env from 'react-dotenv';
 import { Provider } from 'react-redux';
 
 import store from './ducks/store';
