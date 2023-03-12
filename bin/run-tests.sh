e9714adb21aad867be6ea9c8bc10c1aa54f1874a
Mateusz Stapaj <mtxstapaj@gmail.com>
2023-03-12 15:40:09 +0100

    changes in run-tests script

--- bin/run-tests.sh
+++ bin/run-tests.sh
@@ -2,13 +2,12 @@
 
 if docker-compose ls | grep -q 'dev'; then
   echo "Docker Compose development is running."
+  echo "Run tests"
+  cd ../tests/ || (echo "Directory 'tests' doesn't exist. Exiting." && exit)
+  yarn test
 else
   echo "Docker Compose development is not running."
-  echo "Docker Compose development is starting... Wait 30 seconds"
-  ./start-development.sh &
-  sleep 30
+  echo "You need to run docker compose development first."
+  echo "You can do this by running start-development.sh (./start-development.sh)"
+  exit
 fi
-
-echo "Run tests"
-cd ../tests/ || echo "Directory 'tests' doesn't exist. Exiting"
-yarn test
