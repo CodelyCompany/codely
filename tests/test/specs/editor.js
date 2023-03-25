b70e358b6e56278d47c5155e54733d8760dd904e
Mateusz Stapaj <mtxstapaj@gmail.com>
2023-03-25 21:28:42 +0100

    change test files

--- tests/test/specs/editor.js
+++ tests/test/specs/editor.js
@@ -1,47 +1,47 @@
-// const TitlePage = require('../pageobjects/title.page');
-// const LoginPage = require('../pageobjects/login.page');
-// const MainPage = require('../pageobjects/main.page');
-// const EditorPage = require('../pageobjects/editor.page.js');
-//
-// const runCode = async (code, successful, expectedResult = null) => {
-//   await EditorPage.inputCode(code);
-//   await EditorPage.clickLaunchCode();
-//   expectedResult && expect(await EditorPage.getResult()).toBe(expectedResult);
-//   expect(await EditorPage.getSnackbarResult()).toBe(successful);
-// };
-//
-// describe('Editor Test', () => {
-//   it('Should login with valid credentials', async () => {
-//     await TitlePage.open();
-//     await TitlePage.clickLoginButton();
-//     await LoginPage.login('admin@example.com', 'AdminAdmin123');
-//     expect(await MainPage.getUsernameInfo()).toBe('admin');
-//   });
-//
-//   it('Should open editor', async () => {
-//     await MainPage.clickEditorButton();
-//     expect((await driver.getUrl()).slice(-7)).toBe('/editor');
-//   });
-//
-//   it('Should run code successful python', async () => {
-//     await EditorPage.selectPythonLanguage();
-//     await runCode('print("LOL")', true, 'LOL');
-//   });
-//
-//   it('Should run code successful javascript multiline', async () => {
-//     await EditorPage.selectJavascriptLanguage();
-//     await runCode(
-//       'console.log("TEST");\nconsole.log("LINE");',
-//       true,
-//       'TEST\nLINE'
-//     );
-//   });
-//
-//   it('Should fail javascript', async () => {
-//     await runCode('console.log())', false);
-//   });
-//
-//   it('Should run code successful, without text to check', async () => {
-//     await runCode('console.log("T")', true);
-//   });
-// });
+const TitlePage = require('../pageobjects/title.page');
+const LoginPage = require('../pageobjects/login.page');
+const MainPage = require('../pageobjects/main.page');
+const EditorPage = require('../pageobjects/editor.page.js');
+
+const runCode = async (code, successful, expectedResult = null) => {
+  await EditorPage.inputCode(code);
+  await EditorPage.clickLaunchCode();
+  expectedResult && expect(await EditorPage.getResult()).toBe(expectedResult);
+  expect(await EditorPage.getSnackbarResult()).toBe(successful);
+};
+
+describe('Editor Test', () => {
+  it('Should login with valid credentials', async () => {
+    await TitlePage.open();
+    await TitlePage.clickLoginButton();
+    await LoginPage.login('admin@example.com', 'AdminAdmin123');
+    expect(await MainPage.getUsernameInfo()).toBe('admin');
+  });
+
+  it('Should open editor', async () => {
+    await MainPage.clickEditorButton();
+    expect((await driver.getUrl()).slice(-7)).toBe('/editor');
+  });
+
+  it('Should run code successful python', async () => {
+    await EditorPage.selectPythonLanguage();
+    await runCode('print("LOL")', true, 'LOL');
+  });
+
+  it('Should run code successful javascript multiline', async () => {
+    await EditorPage.selectJavascriptLanguage();
+    await runCode(
+      'console.log("TEST");\nconsole.log("LINE");',
+      true,
+      'TEST\nLINE'
+    );
+  });
+
+  it('Should fail javascript', async () => {
+    await runCode('console.log())', false);
+  });
+
+  it('Should run code successful, without text to check', async () => {
+    await runCode('console.log("T")', true);
+  });
+});
--- tests/test/specs/login.js
+++ tests/test/specs/login.js
@@ -1,81 +1,81 @@
-// const TitlePage = require('../pageobjects/title.page');
-// const LoginPage = require('../pageobjects/login.page');
-// const MainPage = require('../pageobjects/main.page');
-// require('dotenv').config();
-//
-// const randomLogin =
-//   'testusercodely' + new Date().toLocaleString().replace(/[/:\s,]/g, '');
-// const randomEmail = randomLogin + '@gmail.example.com';
-// const data = process.env;
-//
-// describe('Login Test', () => {
-//   it('Should not login without credentials', async () => {
-//     await TitlePage.open();
-//     await TitlePage.clickLoginButton();
-//     await LoginPage.login('', '');
-//     expect(await browser.getTitle()).toBe('Log in | Codely-Frontend');
-//   });
-//
-//   it('Should not login with only login', async () => {
-//     await LoginPage.login(data.ADMIN_LOGIN, '');
-//     expect(await browser.getTitle()).toBe('Log in | Codely-Frontend');
-//   });
-//
-//   it('Should not login with only password', async () => {
-//     await LoginPage.login('', data.ADMIN_PASSWORD);
-//     expect(await browser.getTitle()).toBe('Log in | Codely-Frontend');
-//   });
-//
-//   it('Should login with valid credentials', async () => {
-//     await LoginPage.login(data.ADMIN_LOGIN, data.ADMIN_PASSWORD);
-//     expect(await MainPage.getUsernameInfo()).toBe('admin');
-//     await MainPage.logout();
-//     expect(await TitlePage.loginButton).toBeDisplayed();
-//   });
-//
-//   it('Should logout admin', async () => {
-//     await MainPage.logoutAdmin();
-//     expect(await TitlePage.loginButton).toBeDisplayed();
-//   });
-//
-//   it('Should not register without credentials', async () => {
-//     await TitlePage.clickLoginButton();
-//     await LoginPage.clickRegister();
-//     await LoginPage.register('', '');
-//     expect(await browser.getTitle()).toBe('Sign up | Codely-Frontend');
-//   });
-//
-//   it('Should not register with only login', async () => {
-//     await LoginPage.register(randomEmail, '');
-//     expect(await browser.getTitle()).toBe('Sign up | Codely-Frontend');
-//   });
-//
-//   it('Should not register with only password', async () => {
-//     await LoginPage.register('', data.USER_PASSWORD);
-//     expect(await browser.getTitle()).toBe('Sign up | Codely-Frontend');
-//   });
-//
-//   it('Should register with valid credentials', async () => {
-//     await LoginPage.register(randomEmail, data.USER_PASSWORD);
-//     await LoginPage.clickAcceptRegister();
-//     expect(await MainPage.getUsernameInfo()).toBe(randomLogin);
-//   });
-//
-//   it('Should logout user', async () => {
-//     await MainPage.logout();
-//     expect(await TitlePage.loginButton).toBeDisplayed();
-//   });
-//
-//   it('Should login with valid credentials - created user', async () => {
-//     await TitlePage.clickLoginButton();
-//     await LoginPage.login(randomEmail, data.USER_PASSWORD);
-//     expect(await MainPage.getUsernameInfo()).toBe(randomLogin);
-//   });
-//
-//   it('Should logout user', async () => {
-//     await MainPage.logout();
-//     expect(await TitlePage.loginButton).toBeDisplayed();
-//   });
-//
-//   // Testy z logowaniem Google są niemożliwe, ponieważ strona Google wykrywa logowanie za pomocą Selenium i blokuje dostęp
-// });
+const TitlePage = require('../pageobjects/title.page');
+const LoginPage = require('../pageobjects/login.page');
+const MainPage = require('../pageobjects/main.page');
+require('dotenv').config();
+
+const randomLogin =
+  'testusercodely' + new Date().toLocaleString().replace(/[/:\s,]/g, '');
+const randomEmail = randomLogin + '@gmail.example.com';
+const data = process.env;
+
+describe('Login Test', () => {
+  it('Should not login without credentials', async () => {
+    await TitlePage.open();
+    await TitlePage.clickLoginButton();
+    await LoginPage.login('', '');
+    expect(await browser.getTitle()).toBe('Log in | Codely-Frontend');
+  });
+
+  it('Should not login with only login', async () => {
+    await LoginPage.login(data.ADMIN_LOGIN, '');
+    expect(await browser.getTitle()).toBe('Log in | Codely-Frontend');
+  });
+
+  it('Should not login with only password', async () => {
+    await LoginPage.login('', data.ADMIN_PASSWORD);
+    expect(await browser.getTitle()).toBe('Log in | Codely-Frontend');
+  });
+
+  it('Should login with valid credentials', async () => {
+    await LoginPage.login(data.ADMIN_LOGIN, data.ADMIN_PASSWORD);
+    expect(await MainPage.getUsernameInfo()).toBe('admin');
+    await MainPage.logout();
+    expect(await TitlePage.loginButton).toBeDisplayed();
+  });
+
+  it('Should logout admin', async () => {
+    await MainPage.logoutAdmin();
+    expect(await TitlePage.loginButton).toBeDisplayed();
+  });
+
+  it('Should not register without credentials', async () => {
+    await TitlePage.clickLoginButton();
+    await LoginPage.clickRegister();
+    await LoginPage.register('', '');
+    expect(await browser.getTitle()).toBe('Sign up | Codely-Frontend');
+  });
+
+  it('Should not register with only login', async () => {
+    await LoginPage.register(randomEmail, '');
+    expect(await browser.getTitle()).toBe('Sign up | Codely-Frontend');
+  });
+
+  it('Should not register with only password', async () => {
+    await LoginPage.register('', data.USER_PASSWORD);
+    expect(await browser.getTitle()).toBe('Sign up | Codely-Frontend');
+  });
+
+  it('Should register with valid credentials', async () => {
+    await LoginPage.register(randomEmail, data.USER_PASSWORD);
+    await LoginPage.clickAcceptRegister();
+    expect(await MainPage.getUsernameInfo()).toBe(randomLogin);
+  });
+
+  it('Should logout user', async () => {
+    await MainPage.logout();
+    expect(await TitlePage.loginButton).toBeDisplayed();
+  });
+
+  it('Should login with valid credentials - created user', async () => {
+    await TitlePage.clickLoginButton();
+    await LoginPage.login(randomEmail, data.USER_PASSWORD);
+    expect(await MainPage.getUsernameInfo()).toBe(randomLogin);
+  });
+
+  it('Should logout user', async () => {
+    await MainPage.logout();
+    expect(await TitlePage.loginButton).toBeDisplayed();
+  });
+
+  // Testy z logowaniem Google są niemożliwe, ponieważ strona Google wykrywa logowanie za pomocą Selenium i blokuje dostęp
+});
