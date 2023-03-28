13065e24e054ee11bdd353c02caa24d82294d532
Mateusz Stapaj <mtxstapaj@gmail.com>
2023-03-28 20:45:53 +0200

    add README, change run-tests.sh

--- bin/run-tests.sh
+++ bin/run-tests.sh
@@ -4,7 +4,7 @@ if docker-compose ls | grep -q 'dev'; then
   echo "Docker Compose development is running."
   echo "Run tests"
   cd ../tests/ || (echo "Directory 'tests' doesn't exist. Exiting." && exit)
-  yarn test
+  yarn test "$@"
 else
   echo "Docker Compose development is not running."
   echo "You need to run docker compose development first."
