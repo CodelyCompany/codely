58538e343d16984b49120131ea8413180a2660e8
Mateusz Stapaj <mtxstapaj@gmail.com>
2023-05-30 21:09:29 +0200

    changes in pipeline

--- tests/wdio_ci.conf.js
+++ tests/wdio_ci.conf.js
@@ -132,16 +132,7 @@ exports.config = {
   // Services take over a specific job you don't want to take care of. They enhance
   // your test setup with almost no effort. Unlike plugins, they don't add new
   // commands. Instead, they hook themselves up into the test process.
-  services: [[TimelineService], 'docker'],
-
-  dockerOptions: {
-    image: 'selenium/standalone-chrome',
-    healthCheck: 'http://localhost:4444',
-    options: {
-      p: ['4444:4444'],
-      shmSize: '2g',
-    },
-  },
+  services: [[TimelineService], 'chromedriver'],
 
   // Framework you want to run your specs with.
   // The following are supported: Mocha, Jasmine, and Cucumber
