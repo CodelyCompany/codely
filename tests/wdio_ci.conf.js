6794c0f45affae57d5f2fcd0293eaf60c35228e3
Mateusz Stapaj <mtxstapaj@gmail.com>
2023-05-30 22:29:22 +0200

    changes in pipeline

--- tests/wdio_ci.conf.js
+++ tests/wdio_ci.conf.js
@@ -76,7 +76,7 @@ exports.config = {
       acceptInsecureCerts: true,
       // We need to extends some Chrome flags in order to tell Chrome to run headless
       'goog:chromeOptions': {
-        args: ['--headless', '--disable-gpu', '--disable-dev-shm-usage'],
+        args: ['--headless', '--disable-gpu'],
       },
     },
   ],
