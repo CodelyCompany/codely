66a7c8e0484a044bd3cc47222c3c2b5a5627ac10
Mateusz Stapaj <mtxstapaj@gmail.com>
2023-06-03 21:46:14 +0200

    fix

--- frontend/src/ducks/user/reducer.js
+++ frontend/src/ducks/user/reducer.js
@@ -7,7 +7,8 @@ export const usersReducer = (state = { users: [] }, action) => {
       return { users: action.payload };
     case types.POST_USER_SUCCESS:
       return { users: _.uniqBy([...state.users, action.payload], '_id') };
-    case types.UPDATE_USER_SUCCESS || types.UPLOAD_AVATAR_SUCCESS:
+    case types.UPDATE_USER_SUCCESS:
+    case types.UPLOAD_AVATAR_SUCCESS:
       return {
         users: [
           ...state.users.filter((usr) => usr._id !== action.payload._id),
